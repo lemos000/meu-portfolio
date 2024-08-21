2f915412dda36a2df53aed3132b661d284b61e4e
lemos000 <lemosgabriel2016@gmail.com>
2024-08-21 18:57:23 -0300

    Ajuste do bug que impedia o site de rodar em prod

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,32 +26,38 @@ export default function Header() {
       </div>
       <div className="text-center p-9 text-white hidden md:flex justify-center space-x-4">
         {Object.entries(options).map(([name, link], index) => (
-           <a
-           key={name}
-           href={link}
-           className="relative p-2 transition-transform duration-200 ease-out hover:text-rosa"
-           onMouseEnter={(e) => {
-             const siblings = Array.from(e.currentTarget.parentNode.children);
-             siblings.forEach((sibling, siblingIndex) => {
-               if (siblingIndex < index) {
-                 sibling.style.transform = `translateX(-10px)`; // Move para a esquerda
-               } else if (siblingIndex > index) {
-                 sibling.style.transform = `translateX(10px)`;
-               }
-             });
-             e.currentTarget.style.transform = `scale(1.3)`; 
-           }}
-           onMouseLeave={(e) => {
-             const siblings = Array.from(e.currentTarget.parentNode.children);
-             siblings.forEach((sibling) => {
-               sibling.style.transform = `translateX(0)`; // Reseta a posição
-             });
-             e.currentTarget.style.transform = `scale(1)`; // Reseta o tamanho
-           }}
-         >
-           {name}
-         </a>
-       ))}
+          <a
+            key={name}
+            href={link}
+            className="relative p-2 transition-transform duration-200 ease-out hover:text-rosa"
+            onMouseEnter={(e) => {
+              const parentNode = e.currentTarget.parentNode;
+              if (parentNode) {
+                const siblings = Array.from(parentNode.children) as HTMLElement[];
+                siblings.forEach((sibling, siblingIndex) => {
+                  if (siblingIndex < index) {
+                    sibling.style.transform = `translateX(-10px)`; // Move para a esquerda
+                  } else if (siblingIndex > index) {
+                    sibling.style.transform = `translateX(10px)`;
+                  }
+                });
+                (e.currentTarget as HTMLElement).style.transform = `scale(1.3)`;
+              }
+            }}
+            onMouseLeave={(e) => {
+              const parentNode = e.currentTarget.parentNode;
+              if (parentNode) {
+                const siblings = Array.from(parentNode.children) as HTMLElement[];
+                siblings.forEach((sibling) => {
+                  sibling.style.transform = `translateX(0)`; // Reseta a posição
+                });
+                (e.currentTarget as HTMLElement).style.transform = `scale(1)`; // Reseta o tamanho
+              }
+            }}
+          >
+            {name}
+          </a>
+        ))}
       </div>
     </div>
   );
