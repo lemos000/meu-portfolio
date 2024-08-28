063cb8bd7c39db423754888146940979638d9bf0
lemos000 <lemosgabriel2016@gmail.com>
2024-08-28 17:28:46 -0300

    Ajustinho de responsividade básica para a página não bugar

--- src/components/Principal/Principal.tsx
+++ src/components/Principal/Principal.tsx
@@ -8,7 +8,7 @@ export default function Principal() {
       <Title />
       <div className="p-10 text-center bg-black text-white">
        <h2 className="font-bold text-4xl">Olá, é um prazer te conhecer</h2> 
-       <p className="px-72 py-14">Sou um estudante de Análise e Desenvolvimento de Sistemas na FIAP, trilhando meu caminho no universo da tecnologia da informação. 
+       <p className=" lg:px-72 py-14">Sou um estudante de Análise e Desenvolvimento de Sistemas na FIAP, trilhando meu caminho no universo da tecnologia da informação. 
        Atualmente, atuo como Analista de Suporte, experiência que me permite desenvolver uma visão ampla e crítica sobre as necessidades dos usuários e as soluções tecnológicas disponíveis.Entusiasta por desenvolvimento de software, busco aprimorar minhas habilidades em linguagens como Java (Spring Boot), Python (Django), JavaScript (Node.js e Next.js). </p>
       </div>
     </div>
